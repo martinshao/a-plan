8d9fa188b5e5d39a403f453acc2956085ea4a25d
Myron <shaogucheng@hotmail.com>
2019-09-03 19:48:21 +0800

    leetcode

--- leetcode/linkedlist/code/linkedlist-creator.js
+++ leetcode/linkedlist/code/linkedlist-creator.js
@@ -3,39 +3,60 @@ function ListNode(val) {
   this.next = null;
 }
 
-function ArrayToLinkedList(array) {
-  if (Array.isArray(array)) {
-    let head = new ListNode();
-    let currentNode = head;
-    for (let i = 0; i < array.length; i++) {
-      if (currentNode.val === undefined) {
-        currentNode.val = array[i];
-      } else {
-        let newNode = new ListNode(array[i]);
-        currentNode.next = newNode;
-        currentNode = newNode;
-      }
-    }
-    return head;
-  } else {
-    throw 'the param is not Array';
-  }
-}
+let c1 = new ListNode(8);
+let c2 = new ListNode(4);
+let c3 = new ListNode(5);
 
-function LinkedListToArray(linkedList) {
-  const array = new Array();
-  let currentNode = linkedList;
+c1.next = c2;
+c2.next = c3;
 
-  while (currentNode) {
-    array.push(currentNode.val);
-    currentNode = currentNode.next;
-    console.info(array);
-  }
-  return array;
-}
+let a1 = new ListNode(4);
+let a2 = new ListNode(1);
 
-let l1 = ArrayToLinkedList([1, 2, 4, 3, 6]);
-let l2 = ArrayToLinkedList([1, 2]);
+a1.next = a2;
+a2.next = c1;
+
+let b1 = new ListNode(5);
+let b2 = new ListNode(0);
+let b3 = new ListNode(1);
+
+b1.next = b2;
+b2.next = b3;
+b3.next = c1;
+ 
+// function ArrayToLinkedList(array) {
+//   if (Array.isArray(array)) {
+//     let head = new ListNode();
+//     let currentNode = head;
+//     for (let i = 0; i < array.length; i++) {
+//       if (currentNode.val === undefined) {
+//         currentNode.val = array[i];
+//       } else {
+//         let newNode = new ListNode(array[i]);
+//         currentNode.next = newNode;
+//         currentNode = newNode;
+//       }
+//     }
+//     return head;
+//   } else {
+//     throw 'the param is not Array';
+//   }
+// }
+
+// function LinkedListToArray(linkedList) {
+//   const array = new Array();
+//   let currentNode = linkedList;
+
+//   while (currentNode) {
+//     array.push(currentNode.val);
+//     currentNode = currentNode.next;
+//     console.info(array);
+//   }
+//   return array;
+// }
+
+// let l1 = ArrayToLinkedList([1,8,4,5]);
+// let l2 = ArrayToLinkedList([1,8,4,5]);
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
