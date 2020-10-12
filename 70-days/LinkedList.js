df3c7dca4e61a67ddc7b9897e504551e0dac58f7
Martin-Shao <shaogucheng@hotmail.com>
2020-10-12 13:42:01 +0800

    70天算法训练

--- 70-days/LinkedList.js
+++ 70-days/LinkedList.js
@@ -6,8 +6,10 @@ class Node {
 }
 
 class LinkedList {
-  constructor() {
-    this.head = new Node('head');
+  constructor(value) {
+    this.head = value ?
+      new Node(value) :
+      new Node('head');
   }
 
   getLast() {
@@ -32,28 +34,28 @@ class LinkedList {
   }
 
   reverse() {
-    // let prev = null;
-    // let curr = this.head;
-    // while (curr !== null) {
-    //   let nodeTemp = curr.next;
-    //   curr.next = prev;
-    //   prev = curr;
-    //   curr = nodeTemp;
-    // }
-    // return prev;
+    let prev = null;
+    let curr = this.head;
+    while (curr !== null) {
+      let nodeTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nodeTemp;
+    }
+    return prev;
 
-    if (this.head == null || this.head.next == null) return this.head;
-    let p = reverse(head.next);
-    this.head.next.next = this.head;
-    this.head.next = null;
-    return p;
+    // if (head == null || head.next == null) return head;
+    // let p = this.reverse(head.next);
+    // head.next.next = head;
+    // head.next = null;
+    // return p;
   }
 }
 
 function ArrayToLinkedList(array) {
   if (Array.isArray(array)) {
-    let linkedList = new LinkedList();
-    for (let i = 0; i < array.length; i++) {
+    let linkedList = new LinkedList(array[0]);
+    for (let i = 1; i < array.length; i++) {
       linkedList.append(array[i])
     }
     return linkedList;
@@ -65,3 +67,48 @@ function ArrayToLinkedList(array) {
 const linkedList = ArrayToLinkedList([1, 2, 3, 4, 5, 6])
 
 console.info(linkedList)
+
+function mergeTwoLists(l1, l2) {
+  const prehead = new Node(-1);
+
+  let prev = prehead;
+  while (l1 != null && l2 != null) {
+    if (l1.value <= l2.value) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = l1 === null ? l2 : l1;
+
+  return prehead.next;
+};
+
+function ArrayToLinkedList2(array) {
+  if (Array.isArray(array)) {
+    let head = new Node();
+    let currentNode = head;
+    for (let i = 0; i < array.length; i++) {
+      if (currentNode.value === undefined) {
+        currentNode.value = array[i];
+      } else {
+        let newNode = new Node(array[i]);
+        currentNode.next = newNode;
+        currentNode = newNode;
+      }
+    }
+    return head;
+  } else {
+    throw 'the param is not Array';
+  }
+}
+
+const l1 = ArrayToLinkedList2([1, 2, 4])
+const l2 = ArrayToLinkedList2([1, 3, 4])
+
+console.info(mergeTwoLists(l1, l2))
